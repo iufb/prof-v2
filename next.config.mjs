8991ede50a6278552d859d6a72895e224060174b
iufb <bashirov3ld2@gmail.com>
2024-10-15 17:00:11 +0500

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin("./src/shared/config/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     formats: ["image/webp"],
     remotePatterns: [
